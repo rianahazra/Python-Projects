ece924d1d11001244c37fbfccde5e166306419c1
rianahazra <rianahazra08@Gmail.com>
2024-12-21 00:22:33 -0500

    trianglesymmetry.py

--- zstar.py
+++ zstar.py
@@ -1,7 +1,9 @@
 '''
-n=int(input("How many stars do you want"))
-for i in range(1,11):
-    print(' '*(n-i) + (str(i)+' ')*(i))
+n = int(input("How much space do you want indented for the triangle? "))
+for i in range(1, 11):
+    # Set the repetition count for the 10th row
+    repeat = 7 if i == 10 else i
+    print(' ' * (n - i) + (str(i) + ' ') * repeat)
 '''
 
 
